9aa970a30609544ac09091e19a490746a2b4a50f
Magzhan05070300 <45003003+Magzhan05070300@users.noreply.github.com>
2022-06-17 23:39:27 +0600

    Update users.py

--- users.py
+++ users.py
@@ -2,27 +2,15 @@ import os
 from flask import Flask, request
 import telebot
 
-TOKEN = '5287178701:AAGqjOQohzho-G0wl48-zYNBCcRxW9JC_ic'
-TOKEN2 = '5497810512:AAFI8DhRu4apgVAdyeID2ppPJSRQ7Oq0UhE'
+TOKEN = '5497810512:AAFI8DhRu4apgVAdyeID2ppPJSRQ7Oq0UhE'
 APP_URL = f'https://usersstudentline.herokuapp.com/{TOKEN}'
-APP_URL2 = f'https://adminstudentsline.herokuapp.com/{TOKEN2}'
 bot = telebot.TeleBot(TOKEN)
-bot2 = telebot.TeleBot(TOKEN2)
 server = Flask(__name__)
 
 @bot.message_handler(commands=['start'])
 def first(message):
-    bot.send_message(message.chat.id, 'Сәлеметсіз бе USER!')
-def second(message):
-    bot2.send_message(message.chat.id, 'Сәлеметсіз бе ADMIN!')
+    bot.send_message(message.chat.id, 'Сәлеметсіз бе!')
 
-
-@server.route('/' + TOKEN2, methods=['POST'])
-def get_message2():
-    json_string2 = request.get_data().decode('utf-8')
-    update2 = telebot.types.Update.de_json(json_string2)
-    bot2.process_new_updates([update2])
-    return '!', 200
 
 @server.route('/' + TOKEN, methods=['POST'])
 def get_message():
@@ -33,17 +21,10 @@ def get_message():
 
 
 @server.route('/')
-def webhook2():
-    bot2.remove_webhook()
-    bot2.set_webhook(url=APP_URL2)
-    return '!', 200
-
-@server.route('/')
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     return '!', 200
-
 
 
 if __name__ == '__main__':

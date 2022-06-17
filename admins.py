53b792d2b4b0719110b05795d84769665143bdd9
Magzhan05070300 <45003003+Magzhan05070300@users.noreply.github.com>
2022-06-17 22:43:19 +0600

    Create admins.py

---
+++ admins.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, request
+import telebot
+
+TOKEN = '5287178701:AAGqjOQohzho-G0wl48-zYNBCcRxW9JC_ic'
+APP_URL = f'https://usersstudentline.herokuapp.com/{TOKEN}'
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def first(message):
+    bot.send_message(message.chat.id, 'Сәлеметсіз бе!')
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return '!', 200
+
+
+@server.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return '!', 200
+
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))

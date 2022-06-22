496b1242ec286282d040151efffeef2a4ee1d7ca
Magzhan05070300 <45003003+Magzhan05070300@users.noreply.github.com>
2022-06-23 00:11:08 +0600

    Update users.py

--- users.py
+++ users.py
@@ -9,7 +9,7 @@ bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 g = github.Github("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")
-user = g.get_user("Magzhan05070300")
+user = g.get_user()
 """
 repo = g.get_repo("users")
 contents = repo.get_contents("database.db", ref="database", branch="main")"""

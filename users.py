5845a1771c6c265de947ae15e43cae693a74c83c
Magzhan05070300 <45003003+Magzhan05070300@users.noreply.github.com>
2022-07-06 16:59:29 +0600

    Update users.py

--- users.py
+++ users.py
@@ -111,7 +111,7 @@ def first(message):
     keyboard = types.ReplyKeyboardMarkup(True, False)
     keyboard.add('Мәзір')
     send = bot.send_message(message.chat.id,
-                            '\U003120E3'+'*Сикинбаев Бауыржан*\n'
+                            '\U003120E3 *Сикинбаев Бауыржан*\n'
                             'В020 Арнайы педагогика\n'
                             'В002 Мектепке дейінгі оқыту және тәрбиелеу\n\n'
                             '*(2) Таипова Зульфия*\n'
